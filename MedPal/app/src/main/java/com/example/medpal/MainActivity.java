f8ae77f94be59a4eb0d2a2779b63cd7ea5647f14
Annabelle <annabelle.whipps@gmail.com>
2019-10-15 15:37:14 +0100

    Logo Added, Activities connected to Main Activity.

--- MedPal/app/src/main/java/com/example/medpal/MainActivity.java
+++ MedPal/app/src/main/java/com/example/medpal/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.medpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login =(Button)findViewById(R.id.logInButton);
+        register =(Button)findViewById(R.id.signUpButton);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToLoginActivity = new Intent(MainActivity.this, LogIn.class);
+                startActivity(goToLoginActivity);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToRegisterActivity = new Intent(MainActivity.this, Register.class);
+                startActivity(goToRegisterActivity);
+            }
+        });
     }
 }
