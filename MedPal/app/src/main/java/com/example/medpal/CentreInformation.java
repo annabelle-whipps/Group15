19fab0d35e03fa67c0f25c7883076066947c9f62
Quentin <quentin.lenoir@etu.unilim.fr>
2019-10-18 14:46:06 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ MedPal/app/src/main/java/com/example/medpal/CentreInformation.java
@@ -0,0 +1,13 @@
+package com.example.medpal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CentreInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_centre_information);
+    }
+}
