ea9a5afba146aa711f77596c8a8c26f46266fd51
Annabelle <annabelle.whipps@gmail.com>
2019-10-18 15:46:26 +0100

    Added Toast to register activity to notify user of successful registration

--- MedPal/app/src/main/java/com/example/medpal/Register.java
+++ MedPal/app/src/main/java/com/example/medpal/Register.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
 
@@ -16,12 +17,12 @@ public class Register extends AppCompatActivity {
 
         Button btnRegister = findViewById(R.id.btnRegister);
 
-
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) { 
                 Intent homePageIntent = new Intent(Register.this, HomePage.class);
                 startActivity(homePageIntent);
+                Toast.makeText(Register.this, "Registration Successful", Toast.LENGTH_SHORT).show();
             }
         });
     }
