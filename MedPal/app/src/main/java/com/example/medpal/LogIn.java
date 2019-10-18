05b3f480ade8267c72f726d246f2b70b9d53b919
Annabelle <annabelle.whipps@gmail.com>
2019-10-18 14:43:21 +0100

    Layout updated for main activity, login and register activities.

--- MedPal/app/src/main/java/com/example/medpal/LogIn.java
+++ MedPal/app/src/main/java/com/example/medpal/LogIn.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LogIn extends AppCompatActivity {
 
@@ -22,6 +23,7 @@ public class LogIn extends AppCompatActivity {
             public void onClick(View v) {
                 Intent homePageIntent = new Intent(LogIn.this, HomePage.class);
                 startActivity(homePageIntent);
+                Toast.makeText(LogIn.this, "Login Successful", Toast.LENGTH_SHORT).show();
             }
         });
     }
