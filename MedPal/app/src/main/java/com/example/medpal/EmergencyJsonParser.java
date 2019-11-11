86b2c48a9474ee323d22d605971f1d8fa443f9f8
Quentin <quentin.lenoir@etu.unilim.fr>
2019-11-11 09:18:46 +0000

    Add comments and removed useless imports + Add dispatch number

--- MedPal/app/src/main/java/com/example/medpal/EmergencyJsonParser.java
+++ MedPal/app/src/main/java/com/example/medpal/EmergencyJsonParser.java
@@ -1,14 +1,9 @@
 package com.example.medpal;
 
-import android.content.Context;
-import android.util.Log;
-import android.widget.Toast;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Member;
 import java.util.Locale;
 
 public class EmergencyJsonParser {
@@ -16,15 +11,16 @@ public class EmergencyJsonParser {
     private String ISOCountryCode;
 
     public EmergencyJsonParser(String ISOCountryCode){
+        //The JSON file has only upper case code so to avoid any mistake we force the upper case on the ISO code of the sim network.
         this.ISOCountryCode = ISOCountryCode.toUpperCase();
     }
 
 
     /**
-     * Converts the JSON encoded string describing locations provided by the MetOffice DataPoint API to Location objects.
+     * Returns a String from the JSON encoded string describing emergency numbers per country provided by the Emergency Numbers API.
      *
-     * @param json The locations JSON encoded string returned by the MetOffice DataPoint API
-     * @return Locations extracted from the json string.
+     * @param json The emergency number JSON encoded string returned by the Emergency Numbers API.
+     * @return The ambulance emergency number from a specific country as a String.
      */
     public String parseEmergencyJson(String json) {
         String number = "";
@@ -37,23 +33,27 @@ public class EmergencyJsonParser {
                 JSONObject country = countryArray.getJSONObject("Country");
                 String ISO = country.getString("ISOCode");
                 if(ISO.equals(this.ISOCountryCode)){
-                    JSONObject ambulance = countryArray.getJSONObject("Ambulance");
+                    JSONObject ambulance = countryArray.getJSONObject("Ambulance"); //We are only interested in Ambulance services
                     JSONArray All = ambulance.getJSONArray("All");
                     number = All.getString(0);
-                    if(number == "null"){
-                        if (countryArray.getBoolean("Member_112")){
+                    if(number == "null"){ //If for any reason the number is not filled
+                        if (countryArray.getBoolean("Member_112")){ //If the country is using the 112 number it will return 112
                             number = "112";
                         } else {
-                            number = "911";
+                            JSONObject dispatch = countryArray.getJSONObject("Dispatch"); //We are only interested in Ambulance services
+                            JSONArray All_bis = ambulance.getJSONArray("All");
+                            if(All_bis.getString(0).equals("null")){
+                                number = "911"; //If not the default 911 is sent
+                            } else {
+                                number = All_bis.getString(0); //Sometimes countries have the same number for every service so we have tor retrieve it
+                            }
                         }
                     }
                     return number;
                 }
             }
-            // add this new Location to the list of them
         } catch (JSONException e) {
-            // should do some more appropriate error handling here
-            e.printStackTrace();
+            return "ERR";
         }
         return number;
     }
